fa4b1cff7eeb3a22948259be22add40d16ee57c2
Bikash Narayan Panda <oksbwn@gmail.com>
2019-02-08 18:02:58 +0530

    Update AWS_Sensors.cpp

--- Mbed Files/AWS_Sensors.cpp
+++ Mbed Files/AWS_Sensors.cpp
@@ -1,30 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2014, 2015 IBM Corp.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Ian Craggs - initial API and implementation and/or initial documentation
- *    Ian Craggs - make sure QoS2 processing works, and add device headers
- *******************************************************************************/
-
- /**
-  This is a sample program to illustrate the use of the MQTT Client library
-  on the mbed platform.  The Client class requires two classes which mediate
-  access to system interfaces for networking and timing.  As long as these two
-  classes provide the required public programming interfaces, it does not matter
-  what facilities they use underneath. In this program, they use the mbed
-  system libraries.
-
- */
-
 #define MQTTCLIENT_QOS1 0
 #define MQTTCLIENT_QOS2 0
 
